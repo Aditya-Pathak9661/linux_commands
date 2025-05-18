c0d801471ea70d252578e2abed7b6d777332616a
Aditya <adityapathak15228@gmail.com>
2025-05-18 11:43:21 +0530

    added a scirpt file

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+ls 
+pwd 
+
+echo "aditya pathak"
+
+ls -lh -a
+
+echo "welcome to shell scripte"
+echo "hello world!"
+
+echo "there are three way to print in terminel:"
+echo " 1. sh  2. bash 3. ./script.sh "
